39e77fb2782e3ef8c1323383e4d5657f8bbda1ad
sergio ribeiro <sergioribeiro782015@gmail.com>
2023-05-20 15:41:27 -0300

    teste formulário do site

--- src/test/java/qaacademy_teste/exercicio_formulario.java
+++ src/test/java/qaacademy_teste/exercicio_formulario.java
@@ -2,6 +2,7 @@ package qaacademy_teste;
 
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -12,14 +13,30 @@ public class exercicio_formulario {
     String preencherEndereco = "//*[@id='basicBootstrapForm']/div[2]/div/textarea";
     String preencherEmail = "//*[@id='eid']/input";
     String preencherTelefone = "//*[@id='basicBootstrapForm']/div[4]/div/input";
+
     String selecionarBotao = "//*[@id='basicBootstrapForm']/div[5]/div/label[1]/input";
     String selecionarBotaoDois = "//*[@id='checkbox2']";
+
+    String inserirFerramentas = "//*[@id='Skills']";
+
+    String abrirListaPais = "//*[@id='basicBootstrapForm']/div[10]/div/span/span[1]";
+    String selecionarIndia = "//*[@id='select2-country-results']/li[6]";
+
+    String selecionarAno = "//*[@id='yearbox']/option[64]";
+    String selecionarMes = "//*[@id='basicBootstrapForm']/div[11]/div[2]/select/option[5]";
+    String selecionarDia = "//*[@id='daybox']/option[25]";
+
+    String inserirPassword = "//*[@id='firstpassword']";
+    String confirmarPassword = "//*[@id='secondpassword']";
+
+    String refreshNaPagina = "//*[@id='Button1']";
 
     @Test
     public void aberturaNavegador() throws InterruptedException{
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.get("https://demo.automationtesting.in/Register.html");
-
+        
         Thread.sleep(2000);
         driver.findElement(By.xpath(preencherNome)).click();
         driver.findElement(By.xpath(preencherNome)).sendKeys("Sergio");
@@ -31,9 +48,37 @@ public class exercicio_formulario {
         driver.findElement(By.xpath(preencherEmail)).sendKeys("sergioribeiro782015@gmail.com");
         driver.findElement(By.xpath(preencherTelefone)).click();
         driver.findElement(By.xpath(preencherTelefone)).sendKeys("11 987933120");
+
         driver.findElement(By.xpath(selecionarBotao)).click();
         driver.findElement(By.xpath(selecionarBotaoDois)).click();
+
+        driver.findElement(By.xpath(inserirFerramentas)).click();
+        driver.findElement(By.xpath(inserirFerramentas)).sendKeys("Java");
+        driver.findElement(By.xpath(inserirFerramentas)).click();
+
+        driver.findElement(By.xpath(abrirListaPais)).click();
+        driver.findElement(By.xpath(selecionarIndia)).click();
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,400)", "");
+
+        driver.findElement(By.xpath(selecionarAno)).click();
+        driver.findElement(By.xpath(selecionarMes)).click();
+        driver.findElement(By.xpath(selecionarDia)).click();
+
+        driver.findElement(By.xpath(inserirPassword)).click();
+        driver.findElement(By.xpath(inserirPassword)).sendKeys("BaguioT@doido");
+        driver.findElement(By.xpath(inserirPassword)).click();
+        driver.findElement(By.xpath(confirmarPassword)).click();
+        driver.findElement(By.xpath(confirmarPassword)).sendKeys("BaguioT@doido");
+        driver.findElement(By.xpath(confirmarPassword)).click();
+
+        Thread.sleep(5000);
+        driver.findElement(By.xpath(refreshNaPagina)).click();
+
     }
 }
 
 //https://demo.automationtesting.in/Register.html site para realizar o teste.
+
+//https://www.browserstack.com/guide/selenium-scroll-tutorial - código scroll javascript
